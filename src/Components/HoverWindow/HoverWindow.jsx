376cd1b53a0c4188a687afb1962e3d3a6d219638
West-Tiwan <xyzmohit.singh@gmail.com>
2024-07-24 01:56:50 +0530

    working prototype

--- src/Components/HoverWindow/HoverWindow.jsx
+++ src/Components/HoverWindow/HoverWindow.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Trash from '../../assets/trash.svg'
 import styles from "./HoverWindow.module.scss";
 
-const HoverWindow = ({type}) => {
+const HoverWindow = ({type, hover, setHover}) => {
     switch (type) {
         case "spelling":
             return (
-                <div className={styles.container}>
+                <div className={`${styles.container} ${(hover ? styles.hidden : '')}`}>
                     <div className={styles.shadow}>
                         <div className={styles.topic}>Spelling error</div>
                         <div className={styles.correction}>Determination</div>
                     </div>
-                    <div className={styles.group}>
+                    <div className={styles.group} onClick={setHover}>
                         <img src={Trash} alt={Trash} />
                         <p>Dismiss</p>
                     </div>
@@ -19,12 +19,12 @@ const HoverWindow = ({type}) => {
             )
         case "correctness":
             return (
-                <div className={styles.container}>
+                <div className={`${styles.container} ${(hover ? styles.hidden : '')}`}>
                     <div className={styles.shadow}>
                         <div className={styles.topic}>Correctness</div>
                         <div className={styles.correction}>..the attention of scientists and...</div>
                     </div>
-                    <div className={styles.group}>
+                    <div className={styles.group} onClick={setHover}>
                         <img src={Trash} alt={Trash} />
                         <p>Dismiss</p>
                     </div>
